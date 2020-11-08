14710ee89a5a3a85e8f36d3a3bb3e79a6886fc0a
Oliver Hofkens <oliver@novemberfive.co>
2020-11-08 12:16:19 +0100

    Selene; Add circular calendars.

--- genart/selene/__init__.py
+++ genart/selene/__init__.py
@@ -1,10 +1,12 @@
 import datetime as dt
 import logging
-from math import pi
+from math import cos, pi, sin
 
 import cairo
 from numpy.random import default_rng
 
+from genart.cairo_util import operator, source
+from genart.color import Color, LinearGradient, RadialGradient
 from genart.geom import points_along_arc
 from genart.util import parse_size
 
@@ -28,13 +30,21 @@ def main(args, config):
     surface = cairo.SVGSurface(str(out_file), width, height)
     ctx = cairo.Context(surface)
 
-    MOONS = 8
+    bg = RadialGradient([Color(0.7, 0.7, 0.7), Color(0.2, 0.2, 0.2)])
+    with source(ctx, bg.to_pattern(width / 2, height / 2, width / 2)):
+        ctx.paint()
+
+    draw_crown(ctx, width / 2, height / 3, width / 10)
+
+    n_moons = rng.integers(6, 12)
     for i, (x, y) in enumerate(
-        points_along_arc(width / 2, height / 2, width / 3, 0, 2 * pi, MOONS), 1
+        points_along_arc(width / 2, height / 2, width / 2.8, 0, 2 * pi, n_moons)
     ):
-        pct_done = i / MOONS
+        pct_done = i / n_moons
         eclipse_pct = (pct_done * 2.0) - 1.0
-        draw_moon(ctx, x, y, width / 10, eclipse_pct)
+        draw_moon(ctx, x, y, width / (1.2 * n_moons), eclipse_pct)
+
+    draw_circular_calendar(ctx, width / 2, height / 2, width / 4)
 
     surface.finish()
 
@@ -46,11 +56,56 @@ def draw_moon(
     radius: float,
     eclipse_pct: float = -1.0,
 ):
+    moon_color = LinearGradient([Color(0.2, 0.2, 0.2), Color(0.7, 0.7, 0.7)])
+    with source(
+        ctx, moon_color.to_pattern(pos_x - radius, pos_y + radius, pos_x, pos_y)
+    ):
+        ctx.arc(pos_x, pos_y, radius, 0, 2 * pi)
+        ctx.fill_preserve()
+        ctx.clip()
+
+    # crater_color = RadialGradient([Color(0.7, 0.7, 0.7), Color(0.2, 0.2, 0.2)])
+    # crater_radius = radius / 10.0
+    # with source(
+    #     ctx,
+    #     crater_color.to_pattern(pos_x, pos_y, crater_radius, crater_radius * 0.7),
+    # ), operator(ctx, cairo.Operator.DARKEN):
+    #     ctx.arc(pos_x, pos_y, crater_radius, 0, 2 * pi)
+    #     ctx.fill()
+
+    with source(ctx, Color(0.0, 0.0, 0.0).to_pattern()):
+        eclipse_cx = pos_x + eclipse_pct * 2.0 * radius
+        ctx.arc(eclipse_cx, pos_y, radius, 0, 2 * pi)
+        ctx.fill()
+        ctx.reset_clip()
+
+
+def draw_crown(ctx: cairo.Context, pos_x: float, pos_y: float, radius: float):
     ctx.arc(pos_x, pos_y, radius, 0, 2 * pi)
     ctx.stroke_preserve()
     ctx.clip()
 
-    eclipse_cx = pos_x + eclipse_pct * 2.0 * radius
-    ctx.arc(eclipse_cx, pos_y, radius, 0, 2 * pi)
+    crown_eclipse = -0.2
+
+    eclipse_cy = pos_y + crown_eclipse * 2.0 * radius
+    ctx.arc(pos_x, eclipse_cy, radius, 0, 2 * pi)
     ctx.fill()
     ctx.reset_clip()
+
+
+def draw_circular_calendar(
+    ctx: cairo.Context, pos_x: float, pos_y: float, radius: float, chunks: int = 12
+):
+    ctx.arc(pos_x, pos_y, radius, 0, 2 * pi)
+    ctx.stroke_preserve()
+
+    ctx.arc(pos_x, pos_y, 0.9 * radius, 0, 2 * pi)
+    ctx.stroke()
+
+    for (start_x, start_y), (end_x, end_y) in zip(
+        points_along_arc(pos_x, pos_y, 0.9 * radius, 0, 2 * pi, chunks),
+        points_along_arc(pos_x, pos_y, radius, 0, 2 * pi, chunks),
+    ):
+        ctx.move_to(start_x, start_y)
+        ctx.line_to(end_x, end_y)
+        ctx.stroke()
