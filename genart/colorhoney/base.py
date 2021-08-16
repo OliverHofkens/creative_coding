8155ef0383e1583af024a33a058dc1a54c8fbc34
Oliver Hofkens <oliver@gorilla.co>
2021-08-16 17:07:11 +0200

    ColorHoney; Finish basic single-line sentences.

--- genart/colorhoney/base.py
+++ genart/colorhoney/base.py
@@ -2,9 +2,10 @@ import cairo
 
 
 class BaseRenderer:
-    def __init__(self, surface: cairo.Surface, scale: int = 10):
+    def __init__(self, surface: cairo.Surface, scale: int = 10, margin: int = 1):
         self.ctx: cairo.Context = cairo.Context(surface)
         self.scale = scale
+        self.margin = margin
 
     def render(self, text: str):
         pass
--- genart/colorhoney/honey.py
+++ genart/colorhoney/honey.py
@@ -1,4 +1,4 @@
-from math import pi, radians, sin
+from math import pi, radians, sin, sqrt
 
 import cairo
 
@@ -10,17 +10,41 @@ from .palette import ALPHABET_PATTERN
 
 
 class ColorHoneyRenderer(BaseRenderer):
-    def __init__(self, surface: cairo.Surface, scale: int = 10):
-        super().__init__(surface, scale)
+    def __init__(self, surface: cairo.Surface, scale: int = 10, *args):
+        super().__init__(surface, scale, *args)
         self.diag_offset = sin(radians(60)) * scale
+        self.diag_margin = sqrt(self.margin / 2)
+        self.blockwidth = self.diag_offset * 2 + self.margin * 2
+
+        # (Translation to baseline of comb, rotation to base)
+        self.orientations = [
+            ((0, 0), radians(30)),
+            ((self.margin / 2, self.diag_margin), radians(90)),
+            ((2 * self.diag_offset + self.margin, 0), radians(-30)),
+            (
+                (
+                    self.diag_offset + self.margin + self.diag_margin,
+                    (-1.5 * self.scale) - self.diag_margin,
+                ),
+                radians(90),
+            ),
+        ]
 
     def render(self, text: str):
-        for i, char in enumerate(text, 1):
-            with cairoctx.translation(self.ctx, i * self.scale, 2 * self.scale):
-                self.letter(char.upper())
+        with cairoctx.translation(self.ctx, self.scale * 3, self.scale * 3):
+            for i, char in enumerate(text):
+                orient_idx = i % 4
+                block_offset = i // 4
+                orient = self.orientations[orient_idx]
+
+                x_offset = block_offset * self.blockwidth + orient[0][0]
+                y_offset = orient[0][1]
+                with cairoctx.translation(self.ctx, x_offset, y_offset):
+                    with cairoctx.rotation(self.ctx, orient[1]):
+                        self.letter(char)
 
     def letter(self, letter: str):
-        color_top, color_bot = ALPHABET_PATTERN[letter]
+        color_top, color_bot = ALPHABET_PATTERN[letter.upper()]
         self.ctx.move_to(0, 0)
         self.triangle(color_bot)
 
