307029752edbf5e4244b92686214b168f34b6934
Oliver Hofkens <oliver@gorilla.co>
2022-05-29 16:14:03 +0200

    PenStudy; Add attractor generation and improve params.

---
+++ rust/penstudy/src/gen.rs
@@ -0,0 +1,22 @@
+use crate::models::Attractor;
+use nannou::prelude::*;
+use rand::prelude::*;
+use rand::thread_rng;
+use rand_distr::{Distribution, Normal, Uniform};
+
+pub fn generate_attractors(num: usize, avg_mass: f32, width: f32, height: f32) -> Vec<Attractor> {
+    let mut rng = thread_rng();
+    let mass_distr = Normal::new(avg_mass, avg_mass / 10.0).unwrap();
+    let x_distr = Uniform::new(-1.0 * width / 2.0, width / 2.0);
+    let y_distr = Uniform::new(-1.0 * height / 2.0, height / 2.0);
+
+    let mut results: Vec<Attractor> = Vec::with_capacity(num);
+
+    for _ in 0..num {
+        results.push(Attractor {
+            pos: Point2::new(x_distr.sample(&mut rng), y_distr.sample(&mut rng)),
+            mass: mass_distr.sample(&mut rng),
+        })
+    }
+    results
+}
--- rust/penstudy/src/main.rs
+++ rust/penstudy/src/main.rs
@@ -3,14 +3,21 @@ use rand::prelude::*;
 
 use models::Attractor;
 
+mod gen;
 mod models;
 
 const GRAVITATIONAL_CONSTANT: f32 = 6.674e-11;
 const PEN_ANGLE_RAD: f32 = std::f32::consts::FRAC_PI_4;
 const PEN_WIDTH: f32 = 10.0;
+const SCALE: f32 = 0.8;
+const SPEEDUP: f32 = 2.0;
 
 fn main() {
-    nannou::app(model).update(update).simple_window(view).run();
+    nannou::app(model)
+        .update(update)
+        .simple_window(view)
+        .size(1920, 1080)
+        .run();
 }
 
 struct Model {
@@ -24,56 +31,48 @@ fn model(app: &App) -> Model {
     let win = app.window_rect();
 
     Model {
-        // TODO: randomize and add more
-        attractors: vec![
-            Attractor {
-                pos: Point2::new(100.0, 100.0),
-                mass: 1e15,
-            },
-            Attractor {
-                pos: Point2::new(-100.0, 100.0),
-                mass: 1e15,
-            },
-            Attractor {
-                pos: Point2::new(10.0, -10.0),
-                mass: 1e15,
-            },
-        ],
+        attractors: gen::generate_attractors(20, 1e16, win.w() * 2.0 / 3.0, win.h() * 2.0 / 3.0),
         pen: Attractor {
             pos: Point2::new(win.left(), win.top()),
-            mass: 1e8,
+            mass: 1e10,
         },
         prev_pos: Point2::new(win.left(), win.top()),
         velocity: Point2::new(win.w() / 30.0, win.h() / -30.0),
     }
 }
 
-fn update(app: &App, model: &mut Model, _update: Update) {
+fn update(app: &App, model: &mut Model, update: Update) {
+    let delta = update.since_last.as_secs_f32() * SPEEDUP;
+
     model.prev_pos = model.pen.pos;
-    model.pen.pos += model.velocity;
+    model.pen.pos += model.velocity * delta;
 
     let force: Point2 = model
         .attractors
         .iter()
-        .map(|att| gravitational_force(&model.pen, att))
+        .map(|att| gravitational_force(&model.pen, att, SCALE))
         .fold(Point2::ZERO, |a, b| a + b);
-    println!("{}", force / model.pen.mass);
-    model.velocity += force / model.pen.mass;
+
+    model.velocity += force / model.pen.mass * delta;
     model.velocity = model
         .velocity
-        .clamp_length(1.0, app.window_rect().len() / 16.0);
+        .clamp_length(1.0, app.window_rect().len() / 4.0);
 }
 
-fn gravitational_force(att1: &Attractor, att2: &Attractor) -> Point2 {
+fn gravitational_force(att1: &Attractor, att2: &Attractor, scale: f32) -> Point2 {
     let conn = att2.pos - att1.pos;
-    let force = GRAVITATIONAL_CONSTANT * (att1.mass * att2.mass / conn.length_squared());
+    let force = GRAVITATIONAL_CONSTANT * (att1.mass * att2.mass / (conn.length_squared() * scale));
     conn.normalize() * force
 }
 
 fn view(app: &App, model: &Model, frame: Frame) {
     let draw = app.draw();
 
-    draw.background().color(BLACK);
+    //draw.background().color(BLACK);
+
+    //for att in &model.attractors {
+    //    draw.ellipse().w(10.0).h(10.0).xy(att.pos);
+    //}
 
     let conn = model.pen.pos - model.prev_pos;
     let pen_diff_angle = PEN_ANGLE_RAD - conn.angle();
@@ -84,7 +83,7 @@ fn view(app: &App, model: &Model, frame: Frame) {
         .start(model.prev_pos)
         .end(model.pen.pos)
         .weight(pen_thck)
-        .color(RED);
+        .color(WHITE);
 
     draw.to_frame(app, &frame).unwrap();
 }
